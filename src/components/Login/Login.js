7d712db48733d7d978e2b68a70ea359732506e44
saurabh-r1 <sidlock2@gmail.com>
2023-11-30 22:55:46 +0530

    Forgot Password Workflow

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -15,9 +15,14 @@ const Login = () => {
 
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
+  const [isForgotPassword, setIsForgotPassword] = useState(false);
 
   const toggle = () => {
     setIsLogin(!isLogin);
+  };
+
+  const toggleForgotPassword = () => {
+    setIsForgotPassword(!isForgotPassword);
   };
 
   const submitHandler = (event) => {
@@ -69,43 +74,84 @@ const Login = () => {
       });
   };
 
-   
+  const forgotPasswordHandler = () => {
+    const enteredEmail = emailInputRef.current.value;
+
+    fetch(`https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyBLKAbRXVsMIF8DYwJjnSGwYrzgHYy3jiU`, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: enteredEmail,
+        requestType: 'PASSWORD_RESET',
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          return res.json().then((data) => {
+            let errorMessage = 'Password reset failed!';
+            if(data && data.error && data.error.message) {
+              errorMessage = data.error.message;
+            }
+            throw new Error(errorMessage);
+          });
+        }
+      })
+      .then((data) => {
+        alert('Password reset email sent successfully. Check your email for instructions.');
+        toggleForgotPassword(); // Switch back to the login screen after sending the reset email
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
+
   return (
     <Container className="mt-5">
       <div className="login-container">
-        <h2 className="login-header">{isLogin ? 'Login' : 'Sign Up'}</h2>
-        <Form onSubmit={submitHandler}>
+        <h2 className="login-header">{isForgotPassword ? 'Forgot Password' : isLogin ? 'Login' : 'Sign Up'}</h2>
+        <Form onSubmit={isForgotPassword ? forgotPasswordHandler : submitHandler}>
           <Form.Group controlId="email">
             <Form.Label>Email:</Form.Label>
-            <Form.Control
-              type="email"
-              required
-              ref={emailInputRef}
-            />
+            <Form.Control type="email" required ref={emailInputRef} />
           </Form.Group>
 
-          <Form.Group controlId="password">
-            <Form.Label>Password:</Form.Label>
-            <Form.Control
-              type="password"
-              required
-              ref={passwordInputRef}
-            />
-          </Form.Group>
+          {!isForgotPassword && (
+            <Form.Group controlId="password">
+              <Form.Label>Password:</Form.Label>
+              <Form.Control type="password" required ref={passwordInputRef} />
+            </Form.Group>
+          )}
+
+          {!isForgotPassword && (
+            <div className="mb-3">
+              <Button variant="link" onClick={toggleForgotPassword}>
+                Forgot Password?
+              </Button>
+            </div>
+          )}
 
           <div>
-          {!isLoading && <Button type="submit">{isLogin ? 'LOGIN': 'CREATE ACCOUNT'}</Button>}
-          {isLoading && <p className='loading'>Sending request....</p>}
+            {!isLoading && !isForgotPassword && <Button type="submit">{isLogin ? 'LOGIN' : 'CREATE ACCOUNT'}</Button>}
+            {!isLoading && isForgotPassword && <Button type="submit">Reset Password</Button>}
+            {isLoading && <p className='loading'>Sending request....</p>}
           </div>
         </Form>
-        <div className="button2">
-        <Button className='bg-white' onClick={toggle}>
-          { isLogin? 'Create New Account':'Login with Existing Account'}
-        </Button>
-        </div>
+
+        {!isForgotPassword && (
+          <div className="button2">
+            <Button className='bg-white' onClick={toggle}>
+              {isLogin ? 'Create New Account' : 'Login with Existing Account'}
+            </Button>
+          </div>
+        )}
       </div>
     </Container>
   );
 };
 
 export default Login;
+
